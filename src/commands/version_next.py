9024cafe651d58f58ade38c08a70358bc9ad4d71
reitmas32 <rafa.zamora.rals@gmail.com>
2022-07-29 09:50:34 -0500

    feat(...): Update current version in documentation

--- src/commands/version_next.py
+++ src/commands/version_next.py
@@ -3,14 +3,14 @@
 ### copyright = Copyright 2020-2022, Next Project 
 ### date = 28/03/2022
 ### license = PSF
-### version = 3.3.1 
+### version = 3.3.4 
 ### maintainer = Rafael Zamora 
 ### email = rafa.zamora.ram@gmail.com 
 ### status = Production
 ######################################################################
 
 # Update 28/03/2022
-VERSION = "3.3.1"
+VERSION = "3.3.4"
 
 #System Packages
 import os
