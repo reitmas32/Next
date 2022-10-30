be6cc824fdaf16d9e8695e12ec552965418afad4
reitmas32 <rafa.zamora.rals@gmail.com>
2022-10-30 14:49:01 -0600

    Clean: clean command by port to hexagonal architecture

--- src/main.py
+++ src/main.py
@@ -10,25 +10,24 @@
 ######################################################################
 
 # Packages Dependencies
-from email.policy import default
 import click
 
 #Local Packages
-import src.commands.info_next as Info_Next
-import src.commands.version_next as Version_Next
-import src.commands.create_next as Create_Next
-import src.commands.build_next as Build_Next
-import src.commands.run_next as Run_Next
-import src.commands.clean_next as Clean_Next
-import src.commands.config_env as Config_Env
-import src.commands.config_get as Config_Get
-import src.commands.config_set as Config_Set
-import src.commands.config_add as Config_Add
-import src.tool.env as ENV_tools
-import src.commands.exce_next as Exce_Next
-import src.commands.use_next as Use_Next
-import src.funcs.tree as Tree_funcs
-import src.tool.types as TYPES_tools
+# import src.commands.info_next as Info_Next
+# import src.commands.version_next as Version_Next
+# import src.commands.create_next as Create_Next
+# import src.commands.build_next as Build_Next
+# import src.commands.run_next as Run_Next
+# import src.commands.clean_next as Clean_Next
+# import src.commands.config_env as Config_Env
+# import src.commands.config_get as Config_Get
+# import src.commands.config_set as Config_Set
+# import src.commands.config_add as Config_Add
+# import src.tool.env as ENV_tools
+# import src.commands.exce_next as Exce_Next
+# import src.commands.use_next as Use_Next
+# import src.funcs.tree as Tree_funcs
+# import src.tool.types as TYPES_tools
 
 # Packages System
 import os
@@ -55,29 +54,20 @@ import os
 
 @click.group()
 def main():
-    ENV_tools.load_env()
     pass
 
 @main.command('info', short_help='view info the Next')
 def info():
-    Info_Next.info()
+    print('info')
 
 @main.command('version', short_help='view version the Next')
 @click.option('--all',default=0, required=False, help='view version of all NextPackages installed <default=0>')
 def version(all: int):
-    # alone Next version
-    if(all == 0):
-        Version_Next.version()
-    # all NextPackages
-    elif(all == 1):
-        Version_Next.version_all()
-    # Error Not show any version
-    else:
-        exit()
+    print('version')
 
 @main.command('check_env', short_help='check env the NextPackages')
 def check_env():
-    Config_Env.check_env()
+    print('check-env')
 
 @main.command('create', short_help='Create a new project of Next', options_metavar='<name> <options>')
 @click.argument('name', required=True, type=str, metavar='')
@@ -98,61 +88,55 @@ def create(
     build_system: str, 
     type_project: str
     ):
-    Create_Next.create(name, build_dir, name_build, build_system_exe, c_compiler, cxx_compiler, build_system, type_project)
+    print('Create')
 
 @main.command('build', short_help='Build a project of Next')
 @click.argument('build_name', default=None, required=False, type=str, metavar='')
 def build(build_name: str):
-    Build_Next.build(build_name)
+    print('Build')
 
 @main.command('run', short_help='Run a project of Next')
 def run():
-    Run_Next.run()
+    print('Run')
 
 @main.command('clean', short_help='Clean a project of Next')
 def clean():
-    Clean_Next.clean()
+    print('Clean')
 
 @main.command('get', short_help='Get property of current Next Project')
 @click.argument('property', required=True, type=str, metavar='property')
 @click.option('--comments',default=True, required=False, type=bool, help='Select name of build')
 def get(property: str, comments: bool):
-    value_of_property = Config_Get.get(property, comments)
+    print('Get')
 
 @main.command('set', short_help='Set property of current Next Project')
 @click.argument('property', required=True, type=str, metavar='property')
 @click.option('--value',default="name", required=True, help='Select value of current Next Project <default=null>')
 def set(property: str, value):
-    Config_Set.set(property, value)
+    print('Set')
 
 @main.command('add', short_help='Add to property of current Next Project')
 @click.argument('property', required=True, type=str, metavar='property')
 @click.option('--value',default="name", required=True, help='Select value of current Next Project <default=null>')
 def add(property: str, value):
-    Config_Add.add(property, value)
+    print('Add')
     
 @main.command('exec', short_help='Excecute a coomand of Project of property <commands>')
 @click.argument('command')
 def exce(command: str):
-    Exce_Next.exce(command)
+    print('Exec')
     
 @main.command('import', short_help='Add new library in current project')
 @click.argument('name_dependencie')
 @click.option('--dir', required=True, type=str, help='Select Dir of dependencie')
 def import_command(name_dependencie: str, dir: str):
-    Use_Next.use_path(name_dependencie, dir)
+    print('Import')
 
 @main.command('tree', short_help='Run a project of Next')
 def tree():
-    this_dir = os.getcwd()
-
-    Tree_funcs.printTree(dir=this_dir)
+    print('Tree')
     
 @main.command('export', short_help='Export library of current project')
 @click.argument('library', required=True, type=str, metavar='libraryPath')
 def export(library):
-    if type(Config_Set.set('library', library)) == TYPES_tools.nullType():
-        Config_Add.add('library', library)
-    
-#if __name__ == "__main__":
-#main()
+    print('Export')
