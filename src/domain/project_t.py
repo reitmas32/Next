f687d52121d80502c8efd5d2ea4766322d918b50
reitmas32 <rafa.zamora.rals@gmail.com>
2022-10-30 14:51:18 -0600

    Feat: Create Project_t class

---
+++ src/domain/project_t.py
@@ -0,0 +1,9 @@
+from src.domain.config_t import Config_t
+
+class Project_t:
+
+    config: Config_t
+    dir: str #TODO Se debe hacer un tipo Dir_t en src.domain.types
+
+    def __init__(self) -> None:
+        pass
