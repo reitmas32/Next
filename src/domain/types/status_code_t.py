066e2c67da577b250a9bc61b78fd7247232439b3
reitmas32 <rafa.zamora.rals@gmail.com>
2022-10-30 23:50:30 -0600

    feat: Create type StatusCode_t

--- src/domain/types/status_code_t.py
+++ src/domain/types/status_code_t.py
@@ -1,7 +1,30 @@
+######################################################################
+# author = Rafael Zamora
+# copyright = Copyright 2020-2022, Next Project
+# date = 30/10/2022
+### license = PSF
+# version = 3.4.0
+# maintainer = Rafael Zamora
+### email = rafa.zamora.ram@gmail.com
+### status = Production
+######################################################################
+
 class StatusCode_t:
-    def __init__(self, code: int) -> None:
+    _code: int
+    _lable: str
+
+    def __init__(self, code: int, lable: str) -> None:
+        self._code = code
+        self._lable = lable
         pass
 
-class StatusCodes:
-    SUCCESFUL = StatusCode_t(0)
-    ERROR = StatusCode_t(0)
+    def __str__(self) -> str:
+        return '{ ' + str(self._code) + ', ' + self._lable + ' }'
+
+    def string(self) -> str:
+        return 'StatusCode: ' + self._code + ', Lable: ' + self._lable 
+
+
+class StatusCodes_e:
+    SUCCESSFUL = StatusCode_t(0, 'SUCCESSFUL')
+    ERROR = StatusCode_t(-1, 'ERROR')
