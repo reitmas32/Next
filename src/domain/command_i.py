6343c566d95af9f2ac37b24837a840669db1ff47
reitmas32 <rafa.zamora.rals@gmail.com>
2022-10-31 10:26:24 -0600

    doc: Add Documentation Header file

--- src/domain/command_i.py
+++ src/domain/command_i.py
@@ -1,9 +1,22 @@
+######################################################################
+### author = Rafael Zamora
+### copyright = Copyright 2020-2022, Next Project
+### date = 31/10/2022
+### license = PSF
+### version = 3.4.0
+### maintainer = Rafael Zamora
+### email = rafa.zamora.ram@gmail.com
+### status = Production
+######################################################################
+
+
 from src.domain.project_t import Project_t
-from src.domain.types.status_code_t import StatusCodes, StatusCode_t
+from src.domain.types.status_code_t import StatusCodes_e, StatusCode_t
+
 
 class Command_i:
     def __init__(self) -> None:
         pass
 
     def exec(project: Project_t) -> StatusCode_t:
-        return StatusCodes.SUCCESFUL
+        return StatusCodes_e.SUCCESSFUL
