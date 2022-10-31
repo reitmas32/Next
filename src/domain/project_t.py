fadac3a5d6adb3f9610d8551b4c213db27f30053
reitmas32 <rafa.zamora.rals@gmail.com>
2022-10-31 10:31:40 -0600

    feat: Modeling Project_t

--- src/domain/project_t.py
+++ src/domain/project_t.py
@@ -1,9 +1,21 @@
+######################################################################
+### author = Rafael Zamora 
+### copyright = Copyright 2020-2022, Next Project 
+### date = 31/10/2022
+### license = PSF
+### version = 3.4.0 
+### maintainer = Rafael Zamora 
+### email = rafa.zamora.ram@gmail.com 
+### status = Production
+######################################################################
+
 from src.domain.config_t import Config_t
+from src.domain.types.dir_t import Dir_t
 
 class Project_t:
 
     config: Config_t
-    dir: str #TODO Se debe hacer un tipo Dir_t en src.domain.types
+    path: Dir_t
 
-    def __init__(self) -> None:
+    def __init__(self, path: Dir_t = Dir_t('') ) -> None:
         pass
