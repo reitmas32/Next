bedce588066d1546adde56f711f21209fdb7dbe6
reitmas32 <rafa.zamora.rals@gmail.com>
2022-11-03 00:07:51 -0600

    feat: Add inheritance to yaml_interface

--- src/ports/yaml/yaml_ruamel.py
+++ src/ports/yaml/yaml_ruamel.py
@@ -9,11 +9,13 @@
 ### status = Production
 ######################################################################
 
+from typing import Any
 import ruamel.yaml
 from io import StringIO
 from pathlib import Path
 
 from src.domain.types.null_smart_t import NullSmart_t
+from src.ports.yaml.yaml_interface import yaml_interface
 
 def represent_of_null(self, data):
     return self.represent_scalar(u'tag:yaml.org,2002:null', u'null')
@@ -21,14 +23,14 @@ def represent_of_null(self, data):
 yaml = ruamel.yaml.YAML()
 yaml.allow_duplicate_keys = True
 
-class yaml_ruamel:
+class yaml_ruamel_port(yaml_interface):
     def __init__(self) -> None:
         # setup loader (basically options)
         # show null
         yaml.representer.add_representer(type(None), represent_of_null)
     
 
-    def object_to_yaml_str(self, obj, options=NullSmart_t) -> str:
+    def object_to_yaml_str(self, obj: Any, options=None) -> str:
         if options == None: options = {}
         string_stream = StringIO()
         yaml.dump(obj, string_stream, **options)
@@ -36,18 +38,18 @@ class yaml_ruamel:
         string_stream.close()
         return output_str
 
-    def yaml_string_to_object(self, string, options=NullSmart_t) -> object:
+    def yaml_string_to_object(self, string: str, options=None) -> object:
         if options == None: options = {}
         return yaml.load(string, **options)
 
     # f->o
-    def yaml_file_to_object(self, file_path, options=NullSmart_t) -> object:
+    def yaml_file_to_object(self, file_path: str, options=None) -> object:
         if options == None: options = {}
         as_path_object = Path(file_path)
         return yaml.load(as_path_object, **options)
 
     # o->f
-    def object_to_yaml_file(self, obj, file_path, options=NullSmart_t) -> object:
+    def object_to_yaml_file(self, obj: Any, file_path: str, options=None) -> object:
         if options == None: options = {}
         as_path_object = Path(Path(file_path))
         with as_path_object.open('w') as output_file:
