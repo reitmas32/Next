15f281791dcbd8ff5c4b85e32824d8c30b1ebf48
reitmas32 <rafa.zamora.rals@gmail.com>
2022-11-03 00:32:05 -0600

    feat: Create Message Handler interface

---
+++ src/ports/messages/message_interface.py
@@ -0,0 +1,33 @@
+######################################################################
+### author = Rafael Zamora 
+### copyright = Copyright 2020-2022, Next Project 
+### date = 03/10/2022
+### license = PSF
+### version = 3.4.0 
+### maintainer = Rafael Zamora 
+### email = rafa.zamora.ram@gmail.com 
+### status = Production
+######################################################################
+
+# Packages Systems
+from src.domain.types.status_code_t import StatusCode_t, StatusCodes_e
+
+
+class message_handdler_i:
+    def __init__(self) -> None:
+        pass
+
+    def message_error(messageStr: str) -> StatusCode_t:
+        return StatusCodes_e.ERROR
+        
+    def message_warning(messageStr: str) -> StatusCode_t:
+        return StatusCodes_e.WARNING
+        
+    def message_successful(messageStr: str) -> StatusCode_t:
+        return StatusCodes_e.SUCCESSFUL
+        
+    def message_info(messageStr: str) -> StatusCode_t:
+        return StatusCodes_e.SUCCESSFUL
+        
+    def message_waiting(messageStr: str) -> StatusCode_t:
+        return StatusCodes_e.SUCCESSFUL
