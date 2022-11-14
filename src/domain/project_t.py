344845e0266d6258c5aeb45370ba00e20e6c6197
reitmas32 <rafa.zamora.rals@gmail.com>
2022-11-13 18:00:56 -0600

    feat: Add Write_config method

--- src/domain/project_t.py
+++ src/domain/project_t.py
@@ -11,8 +11,12 @@
 
 from src.domain.config_t import Config_t
 from src.domain.types.dir_t import Dir_t
+from src.ports.yaml.yaml_ruamel import yaml_ruamel_port
+from src.domain.types.status_code_t import StatusCode_t, StatusCodes_e
 
 from src.ports.yaml.yaml_ruamel import yaml_ruamel_port
+
+import src.tools.file as FILE_TOOL
 
 class Project_t:
 
@@ -22,4 +26,23 @@ class Project_t:
     def __init__(self, path: Dir_t = Dir_t('') ) -> None:
         self.path = path
         self.config = Config_t(self.path, yaml_port=yaml_ruamel_port())
+
+    def write_config(self) -> StatusCode_t:
+
+        try:
+                    
+            # Write Config
+            with open('config.yaml', 'w') as file:
+                #documents = self.config.yaml.dump(config_map, file)
+                y = yaml_ruamel_port()
+                s = y.object_to_yaml_str(self.config._data)
+                file.write(s)
+            
+            # Remplace chars '[' and ']'
+            FILE_TOOL.remplace_in_file("config.yaml", "'[", "[")
+            FILE_TOOL.remplace_in_file("config.yaml", "]'", "]")
+            
+            return StatusCodes_e.SUCCESSFUL
+        except:
+            return StatusCodes_e.ERROR
         
