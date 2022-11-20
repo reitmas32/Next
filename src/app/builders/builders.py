f83cbef9ac4a78c764b7061eae88699b05565097
reitmas32 <rafa.zamora.rals@gmail.com>
2022-11-20 03:06:05 -0600

    feat: Use the DIctSmart_t for more security

--- src/app/builders/builders.py
+++ src/app/builders/builders.py
@@ -1,6 +1,10 @@
-from src.app.builders.cmake import create_cmake, build_cmake
+from src.domain.types.dict_smart_t import DictSmart_t
+from src.app.builders.cmake_t import Cmake_t
 
-base_builders = {}
+base_builders = DictSmart_t('NoneValue')
 
 
-base_builders['cmake'] = {'create': create_cmake, 'build': build_cmake}
+base_builders['cmake'] = Cmake_t()
+#base_builders['basic'] = Basic_t()
+#base_builders['conan'] = Conan_t()
+#base_builders['premake'] = Premake_t()
