8b5a23cdb305d04e7ebb92068b391d7b6906c79c
reitmas32 <rafa.zamora.rals@gmail.com>
2022-11-26 22:27:23 -0600

    feat: Add Basic_t Builder

--- src/app/builders/builders.py
+++ src/app/builders/builders.py
@@ -1,10 +1,12 @@
 from src.domain.types.dict_smart_t import DictSmart_t
 from src.app.builders.cmake_t import Cmake_t
+from src.app.builders.basic_t import Basic_t
+
 
 base_builders = DictSmart_t('NoneValue')
 
 
 base_builders['cmake'] = Cmake_t()
-#base_builders['basic'] = Basic_t()
+base_builders['basic'] = Basic_t()
 #base_builders['conan'] = Conan_t()
 #base_builders['premake'] = Premake_t()
