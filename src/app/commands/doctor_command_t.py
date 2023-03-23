5320cb5fcb6368c31ab321cf008ca2ee6eda7d30
reitmas32 <rafa.zamora.rals@gmail.com>
2023-03-23 03:08:34 -0600

    refactor: Move func to src.tools.os

--- src/app/commands/doctor_command_t.py
+++ src/app/commands/doctor_command_t.py
@@ -14,7 +14,7 @@ from src.domain.types.status_code_t import StatusCodes_e
 
 import src.tools.string as STRING_TOOL
 
-import os
+import src.tools.os as OS_TOOLS
 import subprocess
 INSTALLED = f'✓'
 NOT_INSTALLED = f'×'
@@ -36,24 +36,6 @@ NOT_INSTALLED = f'×'
 """
 
 
-def find_script_on_path(script_tag: str, full_path=False, _start = True, _max_results=10, _equal = False) -> list[str]:
-    match_scripts = []
-    for dir in os.environ['PATH'].split(':'):
-        for script in os.listdir(dir):
-            if len(match_scripts) >= _max_results:
-                return match_scripts
-            if _equal:
-                if script_tag == script:
-                    match_scripts.append(f"{dir}/{script}") if full_path else match_scripts.append(f"{script}")
-            else:
-                if script.find(script_tag) != -1:
-                    if _start:
-                        if script.find(script_tag) == 0:
-                            match_scripts.append(f"{dir}/{script}") if full_path else match_scripts.append(f"{script}")
-                    else:
-                        match_scripts.append(f"{dir}/{script}") if full_path else match_scripts.append(f"{script}")
-    return match_scripts
-
 class DoctorCommand_t(Command_i):
 
     def __init__(self) -> None:
@@ -62,25 +44,25 @@ class DoctorCommand_t(Command_i):
     def exec(self):
         
         # C/C++ Compilers and Tools
-        gcc_compilers_and_tools = find_script_on_path('gcc')
-        clang_compilers_and_tools = find_script_on_path('clang')
-        cl_compilers_and_tools = find_script_on_path('cl', _equal= True)
+        gcc_compilers_and_tools = OS_TOOLS.find_script_on_path('gcc')
+        clang_compilers_and_tools = OS_TOOLS.find_script_on_path('clang')
+        cl_compilers_and_tools = OS_TOOLS.find_script_on_path('cl', _equal= True)
         
         c_compilers_and_tools = STRING_TOOL.listToStr(set(gcc_compilers_and_tools + clang_compilers_and_tools + cl_compilers_and_tools), separator=', ')
         
-        gxx_compilers_and_tools = find_script_on_path('g++')
-        clangxx_compilers_and_tools = find_script_on_path('clang++')
+        gxx_compilers_and_tools = OS_TOOLS.find_script_on_path('g++')
+        clangxx_compilers_and_tools = OS_TOOLS.find_script_on_path('clang++')
         
         cxx_compilers_and_tools = STRING_TOOL.listToStr(set(gxx_compilers_and_tools + clangxx_compilers_and_tools + cl_compilers_and_tools), separator=', ')
         
         # Git
-        git_scripts = find_script_on_path('git', _equal= True)
+        git_scripts = OS_TOOLS.find_script_on_path('git', _equal= True)
         git_version_output = b''
         if len(git_scripts) >= 1:
             git_version_output = subprocess.run(['git', '--version'], capture_output=True).stdout
             
         # Cmake
-        cmake_scripts = find_script_on_path('cmake', _equal= True)
+        cmake_scripts = OS_TOOLS.find_script_on_path('cmake', _equal= True)
         cmake_version_output = b''
         if len(cmake_scripts) >= 1:
             cmake_version_output = subprocess.run(['cmake', '--version'], capture_output=True).stdout
