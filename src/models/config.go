d0437be0e5f6448ab3dfc1cfb0d1c6ce989bd5ba
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-13 19:17:20 -0600

    feat: create Config Model

--- src/models/config.go
+++ src/models/config.go
@@ -13,6 +13,8 @@ type Config struct {
 	Name        string `yaml:"name"`
 	Version     string `yaml:"version"`
 	Description string `yaml:"description"`
+	TypeProject string `yaml:"type_project"`
+	BuildDir    string `yaml:"build_dir"`
 }
 
 func (c *Config) LoadFromFile(filename string) error {
@@ -23,18 +25,48 @@ func (c *Config) LoadFromFile(filename string) error {
 
 	err = yaml.Unmarshal(yamlFile, c)
 	if err != nil {
-		return err
+		return errors.New(fmt.Sprintf("err.Error(): %v in %s\n", err.Error(), filename))
 	}
 
+	err = validateVersion(c.Version)
+	if err != nil {
+		return errors.New(fmt.Sprintf("err.Error(): %v in %s\n", err.Error(), filename))
+
+	}
+
+	err = validateTypeProject(c.TypeProject)
+	if err != nil {
+		return errors.New(fmt.Sprintf("err.Error(): %v in %s\n", err.Error(), filename))
+	}
+
+	return nil
+}
+
+func validateVersion(version string) error {
 	// Define una expresión regular para verificar el formato.
 	validFormat := regexp.MustCompile(`^[0-9]+(\.[0-9]+)*$`)
 
 	// Convierte el texto en una cadena.
-	versionStr := string(c.Version)
+	versionStr := string(version)
 
 	// Verifica si el formato es válido.
 	if !validFormat.MatchString(versionStr) {
-		return errors.New(fmt.Sprintf("The version format is not valid in the %s", filename))
+		return errors.New("The version format is not valid in the")
+	}
+
+	return nil
+}
+
+func validateTypeProject(version string) error {
+	// Define una expresión regular para verificar el formato.
+	validFormat := regexp.MustCompile(`^(executable|static_library|dynamic_library)$`)
+
+	// Convierte el texto en una cadena.
+	versionStr := string(version)
+
+	// Verifica si el formato es válido.
+	if !validFormat.MatchString(versionStr) {
+		return errors.New("The typeProject format is not valid use (executable | static_library | dynamic_library) in the")
 	}
 
 	return nil
@@ -62,4 +94,12 @@ func (c *Config) SaveToFile(filename string) error {
 	}
 
 	return nil
+}
+
+func (c *Config) ToString() (string, error) {
+	yamlBytes, err := yaml.Marshal(c)
+	if err != nil {
+		return "", err
+	}
+	return string(yamlBytes), nil
 }
